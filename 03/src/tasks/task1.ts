98539bb257f416454a0d9c45791b02727f82c258
Karl Nilsson <karl.nilsson@avaloninnovation.com>
2021-12-03 09:25:55 +0100

    December 03

---
+++ 03/src/tasks/task1.ts
@@ -0,0 +1,34 @@
+import read from '../io/file';
+
+const Task = async () => {
+  const binaryNumbers = await read('input2.txt');
+  const binaryNumberList = binaryNumbers.split('\n');
+
+  // Assume all input has the same length
+  const inputLength = binaryNumberList[0].length;
+
+  const gammaArray = Array(inputLength).fill(0);
+
+  binaryNumberList.map((binary) => {
+    gammaArray.map((_, idx) => {
+      if (binary.charAt(idx) === '1') {
+        gammaArray[idx] += 1;
+      } else if (binary.charAt(idx) === '0') {
+        gammaArray[idx] -= 1;
+      }
+      return null;
+    });
+    return null;
+  });
+
+  const gammaBinary = gammaArray.map((gamma) => (gamma > 0 ? 1 : 0));
+  // Epsilon is the inverse of the gamma
+  const epsilonBinary = gammaBinary.map((gamma) => (gamma === 1 ? 0 : 1));
+
+  const gammaNumber = Number.parseInt(gammaBinary.join(''), 2);
+  const epsilonNumber = Number.parseInt(epsilonBinary.join(''), 2);
+
+  return gammaNumber * epsilonNumber;
+};
+
+export default Task;
---
+++ 03/src/tasks/task2.ts
@@ -0,0 +1,51 @@
+import read from '../io/file';
+
+const separateFromBits = (list: Array<string>, index: number) => {
+  const setList: Array<string> = [];
+  const unsetList: Array<string> = [];
+
+  list.map((dataPoint) => {
+    if (dataPoint.charAt(index) === '1') {
+      setList.push(dataPoint);
+    } else if (dataPoint.charAt(index) === '0') {
+      unsetList.push(dataPoint);
+    }
+    return null;
+  });
+
+  return { setList, unsetList };
+};
+
+const Task = async () => {
+  const binaryNumbers = await read('input2.txt');
+  const binaryNumberList = binaryNumbers.split('\n');
+
+  // Assume all input has the same length
+  const inputLength = binaryNumberList[0].length;
+
+  let oxygenList = binaryNumberList;
+  let co2List = binaryNumberList;
+
+  let iterationIndex = 0;
+  while (oxygenList.length > 1) {
+    const { setList, unsetList } = separateFromBits(oxygenList, iterationIndex);
+
+    oxygenList = setList.length < unsetList.length ? unsetList : setList;
+    iterationIndex += 1 % inputLength;
+  }
+
+  iterationIndex = 0;
+  while (co2List.length > 1) {
+    const { setList, unsetList } = separateFromBits(co2List, iterationIndex);
+
+    co2List = setList.length < unsetList.length ? setList : unsetList;
+    iterationIndex += 1 % inputLength;
+  }
+
+  const oxygenNumber = Number.parseInt(oxygenList[0], 2);
+  const co2Number = Number.parseInt(co2List[0], 2);
+
+  return oxygenNumber * co2Number;
+};
+
+export default Task;
