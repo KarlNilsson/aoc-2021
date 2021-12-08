b8441f3a6702fcee0a8a1a76eb49fc384e165078
Karl Nilsson <karl.nilsson@avaloninnovation.com>
2021-12-08 14:36:08 +0100

    December 07

---
+++ 07/src/tasks/task2.ts
@@ -0,0 +1,33 @@
+import read from '../io/file';
+
+const descendingSum = (num: number): number => {
+  if (num === 0) {
+    return num;
+  }
+  return num + descendingSum(num - 1);
+}
+
+const Task = async () => {
+  const data = await read('input2.txt');
+
+  const positions = data.split(',').map((val: string) => Number.parseInt(val, 10));
+  const maxDistance = positions.reduce((prev, val) => val > prev ? val : prev);
+  const minFuelConsumption = {
+    position: 0,
+    fuelConsumption: Number.MAX_VALUE,
+  };
+  
+  for (let targetPosition = 0; targetPosition <= maxDistance; targetPosition += 1) {
+    let totalFuelConsumption = 0;
+    positions.map((position) => {
+      totalFuelConsumption += descendingSum(Math.abs(position - targetPosition));
+    });
+    if (totalFuelConsumption < minFuelConsumption.fuelConsumption) {
+      minFuelConsumption.fuelConsumption = totalFuelConsumption;
+    }
+  }
+  
+  return minFuelConsumption.fuelConsumption;
+};
+
+export default Task;
